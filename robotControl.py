d43a90c5e9bd52b635919e0bcfa0e2545c1f13d4
klsjadhf <56586471+klsjadhf@users.noreply.github.com>
2020-12-26 01:28:39 +0800

    add new functions: turn and travel

--- robotControl.py
+++ robotControl.py
@@ -7,8 +7,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IP_ADDR = "127.0.0.1"
 TCP_PORT = 5005
 
+debug = False   # put true to print debug message
+
 def connect(ip="127.0.0.1", port=5005):
-    print("connecting to", ip, ", port", port)
+    if debug:
+        print("connecting to", ip, ", port", port)
     s.settimeout(2) 
     try:
         s.connect((ip, port))
@@ -16,21 +19,23 @@ def connect(ip="127.0.0.1", port=5005):
         print("error connecting to ip", e)
 
 def disconnect():
-    print("disconnecting")
+    if debug:
+        print("disconnecting")
     s.close()
 
 def receive(buf_Size=1024):
     try:
         data = s.recv(buf_Size)
-        print("received", data)
+        if debug:
+            print("received", data)
         return data
     except:
         print("receive error")
 
 def move(dir="stop", speed=0):
     # connect(IP_ADDR, TCP_PORT)
-
-    print("dir:", dir, "\nspeed:", speed)
+    if debug:
+        print("dir:", dir, "\nspeed:", speed)
 
     #send movement or other commands
     if dir == "stop":
@@ -54,7 +59,8 @@ def move(dir="stop", speed=0):
 
 def get_sensor(sensor):
     # connect(IP_ADDR, TCP_PORT)
-    print("send", sensor.encode())
+    if debug:
+        print("send", sensor.encode())
     s.send(sensor.encode())
     data = receive()
     # disconnect()
@@ -64,7 +70,8 @@ def get_sensor(sensor):
 def col():
     sensor_array = ["front left", "front", "front right", "back"] #arrangement of sensors
     dir = get_sensor("C").decode().split()
-    print("collision data:", dir)
+    if debug:
+        print("collision data:", dir)
     for i in zip(dir, sensor_array):
         if i[0] == '0':     # collision is 0, no collision is 1
             return i[1]
@@ -78,13 +85,15 @@ mag_y_min = 0
 # larger is towards north
 def angle():    # return degrees relative to north clockwise
     mag = get_sensor("M").decode().split()
-    print(mag)
+    if debug:
+        print(mag)
     x_ang = int(mag[0]) # x axis data
     x_ang = ((x_ang - mag_x_min)/(mag_x_max - mag_x_min) - 0.5)*2
     y_ang = int(mag[1]) # x axis data
     y_ang = ((y_ang - mag_y_min)/(mag_y_max - mag_y_min) - 0.5)*2
     ang = (math.atan2(y_ang,x_ang)*180/math.pi)
-    print(x_ang,y_ang, ang)
+    if debug:
+        print(x_ang,y_ang, ang)
     if y_ang < 0:
         return -ang
     else:
@@ -111,4 +120,39 @@ def cal_mag():
             mag_y_max = int(mag[1])
         elif int(mag[1]) < mag_y_min:
             mag_y_min = int(mag[1])
-    print(mag_x_max, mag_x_min, mag_y_max, mag_y_min)
+    if debug:
+        print(mag_x_max, mag_x_min, mag_y_max, mag_y_min)
+
+# turn number of degrees using gyro
+# - is turn left, + is turn right
+def turn(angle):
+    s.send(b"q")    # clear turn angle
+    receive()
+    if angle < 0:
+        while float(get_sensor("Q").decode()) > angle:
+            if debug:
+                print("sensor:", float(get_sensor("Q").decode()), "angle:", angle)
+            move("left")
+            move("stop")
+    else:
+        while float(get_sensor("Q").decode()) < angle:
+            if debug:
+                print("sensor:", float(get_sensor("Q").decode()), "angle:", angle)
+            move("right")
+            move("stop")
+
+# move distance in meters + is foward - is backward
+def travel(dist):
+    s.send(b"d")    # clear distance
+    receive()
+    if dist < 0:
+        while float(get_sensor("D").decode()) > dist:
+            if debug:
+                print("sensor:", float(get_sensor("D").decode()), "dist:", dist)
+            move("back")
+    else:
+        while float(get_sensor("D").decode()) < dist:
+            if debug:
+                print("sensor:", float(get_sensor("D").decode()), "dist:", dist)
+            move("fwd")
+    move("stop")
